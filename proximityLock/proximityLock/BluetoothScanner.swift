c70dc62bead63d666ea282246d16b16c5f6fbc26
William Henrik Behn <behnwilliam@icloud.com>
2025-10-17 17:01:05 +0200

    change check for selected device to use unique uuid from core bluetooth instead of name

--- proximityLock/proximityLock/BluetoothScanner.swift
+++ proximityLock/proximityLock/BluetoothScanner.swift
@@ -23,7 +23,7 @@ struct KalmanFilterRSSI {
     private(set) var P: Double   // uncertainty
     private let Q: Double        // how much rssi should change
     private let R: Double        // noise
-
+    
     init(initialRSSI: Double,
          processNoise: Double = 0.5,
          measurementNoise: Double = 4.0) {
@@ -32,21 +32,20 @@ struct KalmanFilterRSSI {
         self.Q = processNoise
         self.R = measurementNoise
     }
-
-    //math from chatGPT
+    
     mutating func update(measuredRSSI z: Double) -> Double {
         // 1. Predict
         P = P + Q
-
+        
         // 2. Compute Kalman gain
         let K = P / (P + R)
-
+        
         // 3. Update estimate
         x = x + K * (z - x)
-
+        
         // 4. Update uncertainty
         P = (1 - K) * P
-
+        
         return x
     }
 }
@@ -65,13 +64,13 @@ class BluetoothScanner: NSObject, CBCentralManagerDelegate {
     // Id for apple enheter BT advertisement
     let appleLE0: UInt8 = 0x4C
     let appleLE1: UInt8 = 0x00
-
+    
     private(set) var threshold: Double = -65
     private(set) var devices: Set<DeviceItem> = []
     private(set) var selectedDevice: DeviceItem? = nil
     
     let rssiPublisher = PassthroughSubject<Double, Never>()
-
+    
     private var filter = KalmanFilterRSSI(initialRSSI: -70)
     
     let formatter: DateFormatter = {
@@ -79,11 +78,11 @@ class BluetoothScanner: NSObject, CBCentralManagerDelegate {
         f.dateFormat = "HH:mm:ss"
         return f
     }()
-
+    
     override init() {
         super.init()
         manager = CBCentralManager(delegate: self, queue: DispatchQueue(label: "bt.queue"))
-
+        
         
         
         lockCenter.addObserver(
@@ -107,7 +106,7 @@ class BluetoothScanner: NSObject, CBCentralManagerDelegate {
             self?.unlockTime = Date().timeIntervalSince1970
         }
     }
-
+    
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         switch central.state {
         case .poweredOn:     print("Bluetooth ON")
@@ -120,13 +119,13 @@ class BluetoothScanner: NSObject, CBCentralManagerDelegate {
         }
     }
     
-
+    
     func centralManager(_ central: CBCentralManager,
                         didDiscover peripheral: CBPeripheral,
                         advertisementData: [String : Any],
                         rssi RSSI: NSNumber) {
-   
-    
+        
+        
         guard RSSI.intValue != 127 else { return }
         
         guard !isLocked else { return }
@@ -134,51 +133,49 @@ class BluetoothScanner: NSObject, CBCentralManagerDelegate {
         if startTime == nil {startTime = Date().timeIntervalSince1970}
         
         let now = Date().timeIntervalSince1970
-
+        
         if let lt = self.unlockTime, now - lt <= 60 { return }
         
         if let manufacturerKey = advertisementData[CBAdvertisementDataManufacturerDataKey] as? Data,
-            manufacturerKey.count >= 2, manufacturerKey[0] == appleLE0, manufacturerKey[1] == appleLE1 {
+           manufacturerKey.count >= 2, manufacturerKey[0] == appleLE0, manufacturerKey[1] == appleLE1 {
             
-            let name = (advertisementData[CBAdvertisementDataLocalNameKey] as? String)
-            ?? peripheral.name ?? "Unknown"
-            
-            
-            if let name = peripheral.name {
-                devices.insert(DeviceItem(id: peripheral.identifier.uuidString, name: name))
-            }
-            
-            
-            let selectedDeviceName = selectedDevice?.name ?? "no device"
-            
-            if name.lowercased().contains(selectedDeviceName.lowercased()){
-                //print("[][APPLE] RSSI=\(rssi) dBm m name=\(name)")
-                //print("id=\(peripheral.identifier.uuidString)")
+            if let name = (advertisementData[CBAdvertisementDataLocalNameKey] as? String ?? peripheral.name),
+               !name.isEmpty {
                 
-                let smoothed = filter.update(measuredRSSI: RSSI.doubleValue)
                 
-                rssiPublisher.send(smoothed)
+                let currentDevice: DeviceItem = DeviceItem(id: peripheral.identifier.uuidString, name: name)
+                devices.insert(currentDevice)
                 
-                print ("smoothed=\(smoothed) VS normal=\(RSSI.doubleValue)")
                 
-                guard (Date().timeIntervalSince1970 - (startTime ?? 0.0)) > 25 else { return }
-        
-                if smoothed < threshold{
-                    print("LOCKING at \(Date()) rssi=\(smoothed)")
+                if let selected = self.selectedDevice, currentDevice.id == selected.id {
+                    //print("[][APPLE] RSSI=\(rssi) dBm m name=\(name)")
+                    //print("id=\(peripheral.identifier.uuidString)")
                     
-                    startScreenSaver()
+                    let smoothed = filter.update(measuredRSSI: RSSI.doubleValue)
                     
+                    rssiPublisher.send(smoothed)
+                    
+                    print ("smoothed=\(smoothed) VS normal=\(RSSI.doubleValue)")
+                    
+                    guard (Date().timeIntervalSince1970 - (startTime ?? 0.0)) > 25 else { return }
+                    
+                    if smoothed < threshold{
+                        print("LOCKING at \(Date()) rssi=\(smoothed)")
+                        
+                        startScreenSaver()
+                        
+                    }
                 }
             }
         }
     }
     
     func startScanningIfReady() {
-            if manager.state == .poweredOn {
-                manager.scanForPeripherals(withServices: nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
-            }
+        if manager.state == .poweredOn {
+            manager.scanForPeripherals(withServices: nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
         }
-
+    }
+    
     func stopScanning() {
         manager.stopScan()
     }
@@ -196,7 +193,7 @@ func startScreenSaver() {
     let saverPath = "/System/Library/CoreServices/ScreenSaverEngine.app"
     let url = URL(fileURLWithPath: saverPath)
     let config = NSWorkspace.OpenConfiguration()
-
+    
     DispatchQueue.main.async {
         NSWorkspace.shared.openApplication(at: url, configuration: config) { _, error in
             if let error = error {
